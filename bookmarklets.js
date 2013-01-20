ff0c78bc521f37a8174262d0b77a8cf866117fad
Meck <yesmeck@gmail.com>
2013-01-21 00:33:55 +0800

    Url should be decode

--- bookmarklets.js
+++ bookmarklets.js
@@ -5,17 +5,14 @@ document.addEventListener('DOMContentLoaded', function () {
 
   chrome.bookmarks.getTree(function(nodes) {
     getBookmarklets(nodes);
-    var html = '';
     bookmarklets.forEach(function(bookmarklet) {
-      html += '<li><a href="' + bookmarklet.url + '">' + bookmarklet.title + '</a></li>';
-    });
-    document.getElementById('bookmarklets').innerHTML = html;
-    $("#bookmarklets").html(html);
-    $("#bookmarklets a").click(function(event) {
-      var el = $(event.target);
-      var href = el.attr('href');
-      var script = href.substr(11, href.length - 11);
-      chrome.tabs.update(null, {url: href});
+      $('#bookmarklets').append(
+        $('<li></li>').append(
+          $('<a href="#">' + bookmarklet.title + '</a>').click(function(event) {
+            chrome.tabs.update(null, {url: decodeURI(bookmarklet.url)});
+          })
+        )
+      );
     });
   });
 
